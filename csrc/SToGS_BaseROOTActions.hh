e3406fb61eb06b6db19179803f1a054ab8751f54
stezow <stezow@ipnl.in2p3.fr>
2014-05-28 15:08:15 +0200

    start adding basic ROOT events in SToGS

---
+++ csrc/SToGS_BaseROOTActions.hh
@@ -0,0 +1,100 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+
+#ifndef SToGS_BaseROOTActions_h
+#define SToGS_BaseROOTActions_h 1
+
+#include "SToGS_BaseROOT.hh"
+#include "SToGS_BaseROOTEvents.h"
+
+//! SToGS namespace to protect SToGS classes
+namespace SToGS {
+    //!
+    /*!
+     */
+    ParisEventRun : public BaseROOTTreeRun
+    {
+    protected:
+        TTree *fTree;
+    protected:
+        // Events to be filled from G4 to ROOT
+        PEvent fPrimaryEvent;
+        PEvent fEvent;
+        
+    public:
+        ParisEventRun(TTree *tree);
+        virtual ~ParisEventRun()
+        {;}
+        
+        virtual void RecordEvent(const G4Event* evt);
+    };
+
+    //! The ParisUserAction defines the run,event,track and step actions
+    /*!
+     */
+    class ParisUserAction : public SToGS::BaseROOTTreeAction
+    {
+    protected:
+        //! true if one has also photons from scintillations
+        G4int fisOptical;
+        //! emitted optical photons
+        POpticalEvent *fOpticalEventBeg;
+        //! end of optical photons
+        POpticalEvent *fOpticalEventEnd;
+        
+    public:
+        ParisUserAction(G4String conffile = "setup/SToGS_Tree_actions.conf");
+        virtual ~ParisUserAction()
+        {;}
+        
+        virtual G4Run* GenerateRun();
+        
+        //    virtual void BeginOfRunAction(const G4Run * /*therun*/);
+        virtual void EndOfRunAction(const G4Run * /*therun*/);
+        virtual void BeginOfEventAction(const G4Event * /*event*/);
+        //    virtual void EndOfEventAction(const G4Event * /*event*/);
+        virtual void PreUserTrackingAction(const G4Track * /*atrack*/);
+        virtual void PostUserTrackingAction(const G4Track * /*atrack*/);
+    };
+
+    //! The ParisUserActionInitialization is use to init G4 kernel with the actions defined in ParisUserAction
+    /*!
+     */
+    class ParisUserActionInitialization : public SToGS::AllInOneUserActionInitialization<ParisUserAction>
+    {
+    public:
+        ParisUserActionInitialization(G4String conf = "setup/SToGS_Tree_actions.conf", G4String which_gene = "GPS",
+                                      G4String which_gene_opt = "G4Macros/GPSPointLike.mac"):
+            AllInOneUserActionInitialization<ParisUserAction>(conf,which_gene,which_gene_opt)
+        {;}
+        virtual ~ParisUserActionInitialization()
+        {;}
+    };
+    
+} // SToGS Namespace
+
+#endif
+
